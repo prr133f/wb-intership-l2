52ea971286dcbac20444c4021a10d9af166d1ca9
prr133f <dkrilov2005@gmail.com>
2024-07-03 02:21:34 +0300

    Add file logging

--- develop/dev10/pkg/logger/logger.go
+++ develop/dev10/pkg/logger/logger.go
@@ -4,8 +4,8 @@ import "go.uber.org/zap"
 
 func NewZap() *zap.Logger {
 	cfg := zap.NewDevelopmentConfig()
-	// cfg.Encoding = "json"
-	// cfg.OutputPaths = []string{"./logs/dev10.log"}
+	cfg.Encoding = "json"
+	cfg.OutputPaths = []string{"./logs/dev10.log"}
 
 	return zap.Must(cfg.Build())
 }
